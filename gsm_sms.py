a71b9d1b73107d5da403c10f4416cd35ec236e70
shvwxyz <155872247+shvwxyz18@users.noreply.github.com>
2024-01-07 16:36:52 +0530

    Update gsm_sms.py

--- gsm_sms.py
+++ gsm_sms.py
@@ -1,21 +1,16 @@
 from gsmmodem.modem import GsmModem
 import time
-
-# Replace these values with your actual port and baud rate
-gsm_port = '/dev/ttyUSB0'  # Example port, adjust as needed
-gsm_baudrate = 9600  # Example baud rate, adjust as needed
+gsm_port = '/dev/ttyUSB0' 
+gsm_baudrate = 9600 
 
 def send_sms(message, recipient_number):
     modem = GsmModem(gsm_port, gsm_baudrate)
-    modem.connect()
-    message = 'mg'
-    # Send SMS
     modem.send_sms(recipient_number, message)
-    print(f"SMS sent to {recipient_number}: {message}")
+    print("mg")
 
 if __name__ == "__main__":
-    # Replace these values with the actual message and recipient number
-    message_to_send = "Hello, this is a test SMS from Raspberry Pi!"
-    recipient_phone_number = "9398739018"  # Replace with the recipient's phone number
-    
+    message_to_send = "bane extralu dengutunnav ga"
+    recipient_phone_number = "9876543210" 
     send_sms(message_to_send, recipient_phone_number)
+
+#the gsm module should be attached to the RaspberryPi and run this program. 
