e3780d20fae2df622b3e222db0964cfc0bb13cf5
shvwxyzDaddy <155872247+shvwxyzDaddy@users.noreply.github.com>
2024-01-07 12:37:22 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,3 +7,5 @@ while 1 :
     temp = sensor.get_temperature()
     print('Temperature is {}'.format(temp))
     sleep(1)
+
+#the VCC must be connected to pin no 2, DATA must be connected to pin no 7 and GND to pin no 9
