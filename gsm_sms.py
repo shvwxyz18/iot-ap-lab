41441f0fd2978f540bb3ceec71c288e2a884cffb
shvwxyz <155872247+shvwxyz18@users.noreply.github.com>
2024-01-07 16:47:33 +0530

    Update gsm_sms.py

--- gsm_sms.py
+++ gsm_sms.py
@@ -1,15 +1,9 @@
 from gsmmodem.modem import GsmModem
-gsm_port = '/dev/ttyUSB0' 
+gsm_port = '/dev/ttyUSB0'  
 gsm_baudrate = 9600 
-
-def send_sms(message, recipient_number):
-    modem = GsmModem(gsm_port, gsm_baudrate)
-    modem.send_sms(recipient_number, message)
-    print("mg")
-
-if __name__ == "__main__":
-    message_to_send = "bane extralu dengutunnav ga"
-    recipient_phone_number = "9876543210" 
-    send_sms(message_to_send, recipient_phone_number)
-
-#the gsm module should be attached to the RaspberryPi and run this program. 
+message = "bane extralu dengutunnav ga"
+recipient_number = "9398739018"  
+modem = GsmModem(gsm_port, gsm_baudrate)
+modem.connect()
+modem.send_sms(recipient_number, message)
+print(f"SMS sent to {recipient_number}: {message}")
