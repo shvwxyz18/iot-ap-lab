9ed9a428454f5c714a2188f3a23882ebf337b52b
shvwxyz <155872247+shvwxyz18@users.noreply.github.com>
2024-01-07 17:48:18 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -2,19 +2,15 @@ import cv2
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 video_capture = cv2.VideoCapture(0)
 
-def detect_bounding_box(vid):
+while True:
+    result, vid = video_capture.read() 
+    if not result:
+        break  
     gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
     for (x, y, w, h) in faces:
         cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
-    return faces
-
-while True:
-    result, video_frame = video_capture.read() 
-    if not result:
-        break  
-    faces = detect_bounding_box(video_frame) 
-    cv2.imshow("My Face Detection Project", video_frame) 
+    cv2.imshow("My Face Detection Project", vid) 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
     a = len(faces)
