0d5c84e281c335ae0f38dec627ab7c413219edd7
shvwxyz <155872247+shvwxyz18@users.noreply.github.com>
2024-01-07 17:20:59 +0530

    Update ir_sensor.py

--- ir_sensor.py
+++ ir_sensor.py
@@ -3,7 +3,7 @@ from time import sleep
 
 gp.setmode(gp.BCM)
 
-gp.setup(18, gp.OUT)
+gp.setup(37, gp.OUT)
 gp.setup(16, gp.IN)
 
 while 1:
@@ -13,3 +13,6 @@ while 1:
     else :
         gp.output(gp.LOW)
     sleep(1)
+
+#the +ve end of LED must be connected to pin no 37 and -ve end to pin no 39(refer the pin diagram in the repository)
+#the data of ir sensor must be connected to pin no 16, GND to no 6 and VCC to pin no 4
