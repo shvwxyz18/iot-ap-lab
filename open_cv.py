edc8a29e7f468519d25c1fe5d496ae18a40b0ded
shvwxyz <155872247+shvwxyz18@users.noreply.github.com>
2024-01-07 15:57:19 +0530

    Add files via upload

---
+++ open_cv.py
@@ -0,0 +1,10 @@
+import cv2
+cam = cv2.VideoCapture(0) 
+result, image = cam.read() 
+if result: 
+	cv2.imshow("test", image) 
+	cv2.imwrite("test.png", image) 
+	cv2.waitKey(0) 
+	cv2.destroyWindow("test") 
+else: 
+	print("No image detected. Please! try again") 
